e21106e1bebc2cab6cefef9cc2c7d8bdfc9d360d
kurylin-eugen <oriks91@list.ru>
2024-05-29 20:50:59 +0300

    Delete hw-02 directory

--- hw-02/BMI калькулятор.py
+++
@@ -1,9 +0,0 @@
-print('Введите ваш вес')
-Weight =int(input())
-print('Введите ваш рост')
-Heihgt =int(input())
-BMI= Weight / ((Heihgt/100)**2)
-print('Ваш индекс массы тела' , BMI )
-I = round(BMI)
-L, R = 20, 50
-print(str(L) + '='*(I-L-1) + '|' + '='*(R-I-1) + str(R))
